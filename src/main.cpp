9ec4ecf38dc946afd8df22da749db79b051bfa21
jduga002 <jduga002@ucr.edu>
2015-01-17 14:42:46 -0800

    clean up code; beginning code for executing commands

--- src/main.cpp
+++ src/main.cpp
@@ -16,12 +16,19 @@ int main() {
     while (strcmp(line, "exit") != 0) {
         vector<char *> v_commands;
         v_commands.push_back(strtok(line, ";"));
-        //cout << v_commands.at(0) << endl;
         while (v_commands.at(v_commands.size()-1) != NULL) {
             v_commands.push_back(strtok(NULL, ";"));
         }
-        cout << "$ ";
+
+        for (int i = 0; i < v_commands.size(); i++) {
+            //run the command between each semicolon
+            exec_commands(v_commands.at(i));
+        }
+
+        cout << "$ " << flush;
         cin.getline(line, 500);
     }
+
     return 0;
+
 }
