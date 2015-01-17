b4f58c1fd9d719d43dcfaac4ae5ef71570582cfa
jduga002 <jduga002@ucr.edu>
2015-01-17 14:24:55 -0800

    Filter line between separate semicolons

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,22 @@
 #include <vector>
 using namespace std;
 
+void exec_commands(char *commands) {
+    
+    }
+
 int main() {
     char line[500]; 
     cout << "$ ";
     cin.getline(line, 500);
 
     while (strcmp(line, "exit") != 0) {
-        vector<char *> commands;
+        vector<char *> v_commands;
+        v_commands.push_back(strtok(line, ";"));
+        //cout << v_commands.at(0) << endl;
+        while (v_commands.at(v_commands.size()-1) != NULL) {
+            v_commands.push_back(strtok(NULL, ";"));
+        }
         cout << "$ ";
         cin.getline(line, 500);
     }
