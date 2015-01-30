057391e7deb04d2098c0095393f11a2a9e0daae6
jduga002 <jduga002@ucr.edu>
2015-01-30 13:42:51 -0800

    moved functionality of ls to separate function

--- src/ls.cpp
+++ src/ls.cpp
@@ -7,15 +7,8 @@
 
 using namespace std;
 
-/*
- * This is a BARE BONES example of how to use opendir/readdir/closedir.
- * Notice that there is no error checking on these functions.
- * You MUST add error checking yourself.
- */
-
-int main()
-{
-    DIR *dirp = opendir(".");
+void ls() {
+DIR *dirp = opendir(".");
     if (dirp == NULL) {
         perror("opendir failed");
         exit(1);
@@ -24,13 +17,19 @@ int main()
     while ((direntp = readdir(dirp))) {
         cout << direntp->d_name << endl;  // use stat here to find attributes of file
     }
-    if (errno == EBADF) {  // checks to see if readdir exited loop (returned NULL) because of error
+    if (errno == EBADF) {  // checks if readdir exited loop (returned NULL) because of error
         perror("readdir failed");
         exit(1);
     }
 
     if (-1 ==  closedir(dirp)) {
         perror("closedir failed");
+        exit(0);
     }
+}
+
+int main() {
+    ls();
+    return 0;
 }
 
