f3eb97e6480e9aa4df0b7fb0eec68c37515abcf5
jduga002 <jduga002@ucr.edu>
2015-01-31 20:12:56 -0800

    beginnings of -R flag. Non_functional

--- src/ls.cpp
+++ src/ls.cpp
@@ -1,10 +1,13 @@
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <iostream>
 #include <vector>
+#include <string.h>
 #include <string>
 
 int PROGRAM_SUCCESS = 0;
@@ -12,6 +15,8 @@ int PROGRAM_SUCCESS = 0;
 using namespace std;
 
 void ls(string dir, bool &mult_args, bool &show_all, bool &not_first, bool &is_recursive) {
+    struct stat statbuf;
+    vector<string> v_dirs;
     if (not_first) {
         cout << endl;
     }
@@ -31,6 +36,18 @@ void ls(string dir, bool &mult_args, bool &show_all, bool &not_first, bool &is_r
         if (direntp->d_name[0] != '.' || show_all) {
             cout << direntp->d_name << endl;  // use stat here to find attributes of file
         }
+        if (is_recursive) {
+            if (stat(direntp->d_name, &statbuf) == -1) {
+                perror("stat");
+                exit(1);
+            }
+            if (S_ISDIR(statbuf.st_mode)) {
+                if (direntp->d_name[0] != '.') v_dirs.push_back(direntp->d_name);
+                else if (show_all && 
+                       !(strcmp(direntp->d_name,".")==0 || strcmp(direntp->d_name,"..")==0)) 
+                    v_dirs.push_back(direntp->d_name);
+            }
+        }
     }
     if (errno == EBADF) {  // checks if readdir exited loop (returned NULL) because of error
         perror("readdir failed");
@@ -40,6 +57,8 @@ void ls(string dir, bool &mult_args, bool &show_all, bool &not_first, bool &is_r
     if (-1 ==  closedir(dirp)) {
         perror("closedir failed");
     }
+    for (unsigned i = 0; i < v_dirs.size(); i++)
+        ls(v_dirs.at(i), mult_args, show_all, not_first, is_recursive);
 }
 
 int main(int argc, char **argv) {
